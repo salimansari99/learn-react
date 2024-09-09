67006e2df5378665a99333e1a4646cf7509bff47
Salim Ansari <salim.ansari@nw18.com>
2024-09-09 17:55:38 +0530

    react router

---
+++ 07ReactRouter/src/components/Index.js
@@ -0,0 +1,10 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Github, {githubInfoLoader} from "./Github/Github";
+import Home from "./Home/Home";
+import User from "./User/User";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+
+export {About, Contact, Github, githubInfoLoader, Home, User, Header, Footer};
