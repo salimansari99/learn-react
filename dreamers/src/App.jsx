3a38f560e15d92645a6179b32ea65db48a223622
Salim Ansari <salimansari@MACN-SAAN-SB.local>
2024-09-21 19:22:46 +0530

    appwrite setup

---
+++ dreamers/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <div>
+        <h1>Welcome to Dreamers App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
